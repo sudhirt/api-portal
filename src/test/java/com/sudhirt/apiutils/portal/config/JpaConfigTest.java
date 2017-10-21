6990c2f47e113c0e240bbe4d9e7f1d35780f7a91
Sudhir <sudhirtumati@outlook.com>
2017-10-21 10:59:19 +0800

    feat: added new test to verify that jpa auditing is working properly

---
+++ src/test/java/com/sudhirt/apiutils/portal/config/JpaConfigTest.java
@@ -0,0 +1,28 @@
+package com.sudhirt.apiutils.portal.config;
+
+import com.sudhirt.apiutils.portal.entity.Api;
+import com.sudhirt.apiutils.portal.helper.ApiDataHelper;
+import com.sudhirt.apiutils.portal.service.ApiService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JpaConfigTest {
+
+    @Autowired
+    private ApiService apiService;
+
+    @Test
+    public void created_by_populated_for_saved_entities() {
+        Api savedApi = apiService.save(ApiDataHelper.createApi(1)
+                .iterator()
+                .next());
+        assertThat(savedApi).isNotNull();
+    }
+}

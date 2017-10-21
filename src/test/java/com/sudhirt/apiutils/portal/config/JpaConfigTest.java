db755ac9624d172b39405c084927d57e5c80aca6
Sudhir <sudhirtumati@outlook.com>
2017-10-21 11:05:45 +0800

    fix: corrected failing test case

--- src/test/java/com/sudhirt/apiutils/portal/config/JpaConfigTest.java
+++ src/test/java/com/sudhirt/apiutils/portal/config/JpaConfigTest.java
@@ -9,10 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.transaction.Transactional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Transactional
 public class JpaConfigTest {
 
     @Autowired

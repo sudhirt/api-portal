a89294de17e06d6ab225ad504cda5df0986e820a
Sudhir Tumati <mailofsudhir@gmail.com>
2017-10-21 11:08:27 +0800

    feat: added new test to verify that jpa auditing is working properly
    added new test to verify that jpa auditing is working properly

---
+++ src/test/java/com/sudhirt/apiutils/portal/helper/ApiDataHelper.java
@@ -0,0 +1,29 @@
+package com.sudhirt.apiutils.portal.helper;
+
+import com.sudhirt.apiutils.portal.constant.ApiStatus;
+import com.sudhirt.apiutils.portal.entity.Api;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class ApiDataHelper {
+
+    public static Collection<Api> createApi(int count) {
+        return createApi(count, ApiStatus.INACTIVE, "beta1");
+    }
+
+    public static Collection<Api> createApi(int count, ApiStatus status, String version) {
+        Collection<Api> apis = new ArrayList<>();
+        Api api;
+        for(int index=1; index <= count; index++) {
+            api = new Api();
+            api.setApplication("TEST_" + index);
+            api.setContact("test\"+index+\"@test\"+index+\".com");
+            api.setStatus(status);
+            api.setVersion(version);
+            api.setSwaggerJson("{'key':'value'}");
+            apis.add(api);
+        }
+        return apis;
+    }
+}
--- src/test/java/com/sudhirt/apiutils/portal/service/ApiServiceTest.java
+++ src/test/java/com/sudhirt/apiutils/portal/service/ApiServiceTest.java
@@ -2,6 +2,7 @@ package com.sudhirt.apiutils.portal.service;
 
 import com.sudhirt.apiutils.portal.constant.ApiStatus;
 import com.sudhirt.apiutils.portal.entity.Api;
+import com.sudhirt.apiutils.portal.helper.ApiDataHelper;
 import com.sudhirt.apiutils.portal.repository.ApiRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -29,32 +30,8 @@ public class ApiServiceTest {
     @Test
     public void save() {
         assertThat(repository.count()).isEqualTo(0);
-        Api savedApi = service.save(createApi(1).iterator().next());
+        Api savedApi = service.save(ApiDataHelper.createApi(1).iterator().next());
         assertThat(savedApi.getId()).isNotNull();
         assertThat(repository.count()).isEqualTo(1);
-    }
-
-    @Test
-    public void saveAndRetrieve() {
-
-    }
-
-    private Collection<Api> createApi(int count) {
-        return createApi(count, ApiStatus.INACTIVE, "beta1");
-    }
-
-    private Collection<Api> createApi(int count, ApiStatus status, String version) {
-        Collection<Api> apis = new ArrayList<>();
-        Api api;
-        for(int index=1; index <= count; index++) {
-            api = new Api();
-            api.setApplication("TEST_" + index);
-            api.setContact("test\"+index+\"@test\"+index+\".com");
-            api.setStatus(status);
-            api.setVersion(version);
-            api.setSwaggerJson("{'key':'value'}");
-            apis.add(api);
-        }
-        return apis;
     }
 }
